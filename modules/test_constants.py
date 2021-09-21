ef922d8d305988d4346bfbc46f53c1ac599320f9
valentin <valentin-42@mail.ru>
2021-09-21 21:40:39 +0300

    add constants about response messages

--- modules/test_constants.py
+++ modules/test_constants.py
@@ -1,8 +1,18 @@
 BASE_URI = "http://dummy.restapiexample.com/"
 API_REST_VERSION_URI = {1: "api/v1"}
+AVAILABLE_IDS = range(1, 25)
 
 
 class RestPoints:
     """"""
     EMPLOYEE = "employee"
     CREATE = "create"
+
+
+class ResponseMessages:
+    """"""
+    SUCCESS_STATUS = "success"
+    ERROR_STATUS = "error"
+    SUCCESS_MSG = "Successfully! Record has been fetched."
+    NOT_FOUND_MSG = "Not found record"
+    ID_NOT_FOUND_ERR = "id is empty"
